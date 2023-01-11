1be22b78682cbbd21ce3f19c5238dc6d72c88031
bao.ta <tathanhbaobk@gmail.com>
2023-01-11 10:56:03 +0700

    display wrong and correct answer

--- QuizzApp/app/src/main/java/com/example/quizzapp/QuizQuestionActivity.kt
+++ QuizzApp/app/src/main/java/com/example/quizzapp/QuizQuestionActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.View
 import android.view.View.*
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_question.*
 import kotlin.math.log
@@ -25,6 +26,8 @@ class QuizQuestionActivity : AppCompatActivity(), OnClickListener {
     }
 
     private fun setQuestion() {
+        defaultOptionClick()
+        questionNumber.text = "Question $currentIndex:"
         progressBar.progress = currentIndex
         textProgress.text = "${currentIndex}/9"
         image.setImageResource(listQuestion[currentIndex - 1].image)
@@ -38,12 +41,40 @@ class QuizQuestionActivity : AppCompatActivity(), OnClickListener {
         option3.setOnClickListener(this)
         option4.setOnClickListener(this)
         submitButton.setOnClickListener(this)
-        if(currentIndex == 9) {
+        if (currentIndex == 9) {
             submitButton.text = "Finish"
         } else {
             submitButton.text = "Submit"
         }
     }
+
+
+    private fun answerView(answer: Int, drawableView: Int) {
+
+        when (answer) {
+            1 -> {
+                option1.let {
+                    it.background = ContextCompat.getDrawable(this, drawableView)
+                }
+            }
+            2 -> {
+                option2.let {
+                    it.background = ContextCompat.getDrawable(this, drawableView)
+                }
+            }
+            3 -> {
+                option3.let {
+                    it.background = ContextCompat.getDrawable(this, drawableView)
+                }
+            }
+            4 -> {
+                option4.let {
+                    it.background = ContextCompat.getDrawable(this, drawableView)
+                }
+            }
+        }
+    }
+
 
     private fun defaultOptionClick() {
         val options = ArrayList<TextView>()
@@ -77,29 +108,56 @@ class QuizQuestionActivity : AppCompatActivity(), OnClickListener {
 
 
     override fun onClick(v: View?) {
-        when (v?.id){
-            R.id.option1-> {
-                    option1.let {
-                        selectedOptionView(it,1)
-                    }
+        when (v?.id) {
+            R.id.option1 -> {
+                option1.let {
+                    selectedOptionView(it, 1)
+                }
             }
-            R.id.option2-> {
+            R.id.option2 -> {
                 option2.let {
-                    selectedOptionView(it,2)
+                    selectedOptionView(it, 2)
                 }
             }
-            R.id.option3-> {
+            R.id.option3 -> {
                 option3.let {
-                    selectedOptionView(it,3)
+                    selectedOptionView(it, 3)
                 }
             }
-            R.id.option4-> {
+            R.id.option4 -> {
                 option4.let {
-                    selectedOptionView(it,4)
+                    selectedOptionView(it, 4)
                 }
             }
-            R.id.submitButton->{
+            R.id.submitButton -> {
                 submitButton.let {
+
+                    if (selectedOption == 0) {
+                        currentIndex++
+                        when {
+                            currentIndex <= 9 -> {
+                                setQuestion()
+                            }
+                            else -> {
+                                Toast.makeText(this, "You have successfully completed the quiz.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                    } else {
+                        val question = listQuestion[currentIndex-1]
+                        if(question.answer != selectedOption ) {
+                            answerView(selectedOption, R.drawable.wrong_option_border)
+                        }
+
+                        answerView(question.answer, R.drawable.correct_option_border)
+
+                        if(currentIndex == 9) {
+                            it.text = "FINISHED"
+                        } else {
+                            it.text = "GO TO NEXT QUESTION"
+                        }
+                        selectedOption = 0
+                    }
 
                 }
             }
