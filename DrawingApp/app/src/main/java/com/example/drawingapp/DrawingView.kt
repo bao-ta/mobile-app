1435807ec2b0b492b8afd1e8555e9b6eab8af048
bao.ta <tathanhbaobk@gmail.com>
2023-01-12 13:54:21 +0700

    mv

---
+++ DrawingApp/app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -0,0 +1,42 @@
+package com.example.drawingapp
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Camera
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context: Context, attributes: AttributeSet) : View(context, attributes) {
+
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Double = 0.0
+    private var color: Int = Color.BLACK
+    private var canvas: Camera? = null
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.00
+
+    }
+    internal inner class CustomPath(var color: Int, var brushThickness: Double) : Path() {
+
+
+    }
+}
